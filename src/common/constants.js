628e64ab8ed13e04e1c3d0d581d9d5a59c06d998
Cheng Soon Teck <teck.lah@gmail.com>
2026-01-01 21:37:22 +0800

    Configure to cloud endpoints

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,3 +1,3 @@
-export const CREATE_SERVICE_REQUEST_URL = 'http://localhost:8000/service-request';
-export const CREATE_INCIDENT_TICKET_URL = 'http://localhost:8000/incident-ticket';
-export const LOGIN_URL = 'http://localhost:8000/login';
+export const CREATE_SERVICE_REQUEST_URL = 'https://itsm-backend-262012824206.asia-southeast1.run.app/service-request';
+export const CREATE_INCIDENT_TICKET_URL = 'https://itsm-backend-262012824206.asia-southeast1.run.app/incident-ticket';
+export const LOGIN_URL = 'https://itsm-backend-262012824206.asia-southeast1.run.app/login';
