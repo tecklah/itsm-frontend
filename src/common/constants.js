173068c73bd05ab9717a15766222b94db3d63129
Cheng Soon Teck <teck.lah@gmail.com>
2026-01-02 21:33:31 +0800

    Add env config

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,5 +1,7 @@
-export const CREATE_SERVICE_REQUEST_URL = 'https://itsm-backend-262012824206.asia-southeast1.run.app/service-request';
-export const LIST_SERVICE_REQUEST_URL = 'https://itsm-backend-262012824206.asia-southeast1.run.app/service-request';
-export const CREATE_INCIDENT_TICKET_URL = 'https://itsm-backend-262012824206.asia-southeast1.run.app/incident-ticket';
-export const LIST_INCIDENT_TICKET_URL = 'https://itsm-backend-262012824206.asia-southeast1.run.app/incident-ticket';
-export const LOGIN_URL = 'https://itsm-backend-262012824206.asia-southeast1.run.app/login';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const CREATE_SERVICE_REQUEST_URL = `${API_BASE_URL}/service-request`;
+export const LIST_SERVICE_REQUEST_URL = `${API_BASE_URL}/service-request`;
+export const CREATE_INCIDENT_TICKET_URL = `${API_BASE_URL}/incident-ticket`;
+export const LIST_INCIDENT_TICKET_URL = `${API_BASE_URL}/incident-ticket`;
+export const LOGIN_URL = `${API_BASE_URL}/login`;
